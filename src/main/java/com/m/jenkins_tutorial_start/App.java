9a1a34ca795db1f11fb0abdd27a36ddf9660f2e5
Mariusz <mariusz.dolata@capgemini.com>
2018-06-14 15:37:16 +0200

    3 simple tests

---
+++ src/main/java/com/m/jenkins_tutorial_start/App.java
@@ -0,0 +1,23 @@
+package com.m.jenkins_tutorial_start;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( message());
+    }
+    
+    public static String message() {
+    	return "Hi";
+    }
+    public static int returnInt() {
+    	return 1;
+    }
+    public static boolean returnBoolean() {
+    	return true;
+    }
+}
---
+++ src/test/java/com/m/jenkins_tutorial_start/AppTest.java
@@ -0,0 +1,24 @@
+package com.m.jenkins_tutorial_start;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals("Hi", new App().message());
+	}
+	
+	@Test
+	public void returnIntTest() {
+		assertEquals(1, new App().returnInt());
+	}
+	
+	@Test
+	public void returnBooleanTest() {
+		assertEquals(true, new App().returnBoolean());
+	}
+
+}

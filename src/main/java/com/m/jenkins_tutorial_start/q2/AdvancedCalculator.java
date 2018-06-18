b9ee3aac8fbbbdcfaf6dec963082431a8aa5bcd1
Mariusz <mariusz.dolata@capgemini.com>
2018-06-18 10:10:22 +0200

    q2

---
+++ src/main/java/com/m/jenkins_tutorial_start/q2/AdvancedCalculator.java
@@ -0,0 +1,14 @@
+package com.m.jenkins_tutorial_start.q2;
+
+public class AdvancedCalculator {
+	
+	
+	public static double multiplication(double a,double b) {
+		return a*b;
+	}
+	
+	public static double division(double a, double b) {
+		return a*b;
+	}
+
+}
